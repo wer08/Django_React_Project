c4981ce20f173a92a8de5ad946560c1d044735ac
wer08 <wz08061992@gmail.com>
2023-03-22 07:37:53 +0100

    added changing a title when there is unread messade

--- frontend/src/hocs/layout.jsx
+++ frontend/src/hocs/layout.jsx
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import { io } from "socket.io-client";
 import 'react-toastify/dist/ReactToastify.css';
 import { get_users, get_statuses, get_convo } from '../actions/myProject';
+import {Helmet} from "react-helmet"
 
 
 
@@ -13,10 +14,14 @@ const SOCKET_URL = 'http://localhost:8080';
 const Layout = ({check_authentication, load_user, children, get_users, user,get_statuses, users, get_convo, receiver}) => {
 
     const [chatMessage, setChatMessage] = useState("")
+    const [newMessage, setNewMessage] = useState(false)
+    const [sender, setSender] = useState(null)
     const socket = io(SOCKET_URL);
     
     const notify = (message) => {
+        console.log(newMessage)
         const user = users.find(user=>user.id == message.sender)
+        setSender(user)
         const body = message.body.length > 50 ? `${message.body.slice(0,50)}...` : message.body
         toast.info(<><p className='fw-bold ms-2'>
             {user.first_name} {user.last_name}</p>
@@ -24,6 +29,11 @@ const Layout = ({check_authentication, load_user, children, get_users, user,get_
             </>)
     }
     useEffect(()=>{
+        document.addEventListener('visibilitychange',()=>{
+            if(document.visibilityState === 'visible'){
+                setNewMessage(false);
+            }
+        })
         check_authentication();
         load_user();
         get_users();
@@ -52,7 +62,7 @@ const Layout = ({check_authentication, load_user, children, get_users, user,get_
                 get_convo(user.id,receiver,e.page);
                 user.id != e.sender && notify(e)
             }
-
+            setNewMessage(true)
         })
 
         return ()=>{
@@ -64,6 +74,9 @@ const Layout = ({check_authentication, load_user, children, get_users, user,get_
 
     return (  
         <div style={{position: 'relative',height: '87vh'}}>
+            <Helmet>
+                {newMessage ? <title>{`${sender?.first_name}`} {`${sender?.last_name}`} send a message</title> : <title>Communicator</title>}
+            </Helmet>
             <Navbar />
             <ToastContainer />
             {children}
